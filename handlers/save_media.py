6a97440c39106af00580897c77245a65ddac1648
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-02-11 22:33:23 +0530

    Add files via upload

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -89,11 +89,11 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             disable_web_page_preview=True)
         share_link = f"https://t.me/{Config.BOT_USERNAME}?start=LazyDeveloperr_{str_to_b64(file_er_id)}"
         caption_z = f"{message.caption}"
-        thumbs= {message.video.thumbs[0]}
         await editable.edit(
             "**𝘺𝘰𝘶𝘳 𝘍𝘪𝘭𝘦𝘴 𝘚𝘵𝘰𝘳𝘦𝘥 𝘪𝘯 𝘮𝘺 𝘋𝘢𝘵𝘢𝘣𝘢𝘴𝘦!**\n\n"
             f"𝙃𝙚𝙧𝙚 𝙞𝙨 𝙩𝙝𝙚 𝙋𝙚𝙧𝙢𝙖𝙣𝙚𝙣𝙩 𝙇𝙞𝙣𝙠 𝙤𝙛 𝙮𝙤𝙪𝙧 𝙛𝙞𝙡𝙚: {share_link}  \n\n"
             f"𝘑𝘶𝘴𝘵 𝘊𝘭𝘪𝘤𝘬 𝘵𝘩𝘦 𝘭𝘪𝘯𝘬 𝘵𝘰 𝘨𝘦𝘵 𝘺𝘰𝘶𝘳 𝘧𝘪𝘭𝘦...\n\n caption : {caption_z}",
+            photo=thumbs,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
                  [InlineKeyboardButton("ß⊕ts Channel", url="https://t.me/LazyDeveloper"),
@@ -101,8 +101,11 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             ),
             disable_web_page_preview=True
         )
-        await editable.edit(photo=thumbs)
-
+        thumbs= message.video.thumbs[0]
+        file_id= thumbs.file_id
+        location=await bot.download_media(file_id)
+        await message.reply_photo(photo=location)
+        
     except FloodWait as sl:
         if sl.value > 45:
             print(f"Sleep of {sl.value}s caused by FloodWait ...")
