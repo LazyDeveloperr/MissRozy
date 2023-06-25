48b1e4c20e354e57b8461187d45f7007ea145b87
nafps-666 <100076163+nafps-666@users.noreply.github.com>
2023-06-25 11:48:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,14 +2,14 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "5965340120"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001765107260")
+	API_ID = int(os.environ.get("22349232"))
+	API_HASH = os.environ.get("190d65ed459ba1b8e047483914e30756")
+	BOT_TOKEN = os.environ.get("5832681391:AAGJQl0bLLHrg9EWmPnj8wYwmgT4YVQa7pk")
+	BOT_USERNAME = os.environ.get("Ns movies")
+	DB_CHANNEL = int(os.environ.get("Ns_moviez"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "22349232"))
+	DATABASE_URL = os.environ.get("https://t.me/+x1RTNvhp1FM0YmI1")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL",)
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
