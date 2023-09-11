8175835e0012cd57fab800ce0946626469c96072
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-09-11 16:00:32 +0530

    test 14

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -36,10 +36,30 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
             return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                          message_id=file_id)
+                                          message_id=file_id, 
+                                          reply_markup=InlineKeyboardMarkup(
+                                            [
+                                                [
+                                                    InlineKeyboardButton(
+                                                        text="▶ ɢᴇɴ ꜱᴛʀᴇᴀᴍ / ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ", callback_data=f'generate_stream_link:{filez_id}'
+                                                    )
+                                                ]
+                                            ]
+                                            ),
+                                            )
         elif Config.FORWARD_AS_COPY is False:
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                              message_ids=file_id)
+                                              message_ids=file_id,
+                                              reply_markup=InlineKeyboardMarkup(
+                                                [
+                                                    [
+                                                        InlineKeyboardButton(
+                                                            text="▶ ɢᴇɴ ꜱᴛʀᴇᴀᴍ / ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ", callback_data=f'generate_stream_link:{filez_id}'
+                                                        )
+                                                    ]
+                                                ]
+                                                ),
+                                              )
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
