5822df1e3d5265346eb2fdbb61d5e20d709e8c10
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-09-12 17:29:21 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -12,22 +12,12 @@ from urllib.parse import quote_plus
 from util.file_properties import get_name, get_hash, get_media_file_size
 
 async def reply_forward(message: Message, file_id: int):
+
     try:
         await message.reply_text(
             f"**ʜᴇʀᴇ ɪꜱ ꜱʜᴀʀᴀʙʟᴇ ʟɪɴᴋ ᴏꜰ ᴛʜɪꜱ ꜰɪʟᴇ:**\n"
             f"https://t.me/{Config.BOT_USERNAME}?start=LazyDeveloperr_{str_to_b64(str(file_id))}\n"
-            f"__ᴛᴏ ʀᴇᴛʀɪᴠᴇ ᴛʜᴇ ꜱᴛᴏʀᴇᴅ ꜰɪʟᴇ, ᴊᴜꜱᴛ ᴏᴘᴇɴ ᴛʜᴇ ʟɪɴᴋ !__\n\n"
-            f"**✪ ʜᴇʀᴇ ɪꜱ ꜰᴀꜱᴛ ᴅᴏᴡɴʟᴏᴀᴅ ᴀɴᴅ ꜱᴛʀᴇᴀᴍ ʟɪɴᴋ:**\n"
-            f"**[[ ᴊᴏɪɴ ᴅᴇᴠ ᴄʜᴀɴɴᴇʟ ](https://t.me/LazyDeveloper)]** - **[[ Sᵤ𝚋𝘴𝚌ᵣᵢ𝚋ₑ ](https://youtube.com/@LazyDeveloperr)]**\n",
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="▶ ɢᴇɴ ꜱᴛʀᴇᴀᴍ / ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ", callback_data=f'generate_stream_link:{file_id}'
-                    )
-                ]
-            ]
-            ),
+            f"__ᴛᴏ ʀᴇᴛʀɪᴠᴇ ᴛʜᴇ ꜱᴛᴏʀᴇᴅ ꜰɪʟᴇ, ᴊᴜꜱᴛ ᴏᴘᴇɴ ᴛʜᴇ ʟɪɴᴋ !__\n\n",
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
@@ -38,39 +28,50 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
         if Config.FORWARD_AS_COPY is True:
                 lazy_file = await bot.copy_message(chat_id=STREAM_LOGS, from_chat_id=Config.DB_CHANNEL,
                                           message_id=file_id)
-                # lazy_filex = await bot.get_messages(chat_id=Config.DB_CHANNEL, message_ids=file_id)
 
 
                 lazy_stream = f"{URL}watch/{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
                 lazy_download = f"{URL}{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+                
+                fileName = quote_plus(get_name(lazy_file))
 
+                await lazy_file.reply_text(
+                    text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
+                    quote=True,
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("web Download", url=lazy_download),  # we download Link
+                                                        InlineKeyboardButton('▶Stream online', url=lazy_stream)]])  # web stream Link
+                )
                 return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                           message_id=file_id, 
                                           reply_markup=InlineKeyboardMarkup(
                                             [
                                                 [
                                                   InlineKeyboardButton("Fast Download", url=lazy_download),
-                                                  InlineKeyboardButton("watch Online", url=lazy_stream),
-                                                ],
-                                            ]),
-                                            )
-
-                return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                          message_id=file_id, 
-                                          reply_markup=InlineKeyboardMarkup(
-                                            [
-                                                [
-                                                  InlineKeyboardButton("watch", url=lazy_stream),
+                                                  InlineKeyboardButton("▶Stream online", url=lazy_stream),
                                                 ],
                                             ]),
                                             )
         elif Config.FORWARD_AS_COPY is False:
+            lazy_file = await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                              message_ids=file_id)
+            lazy_stream = f"{URL}watch/{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+            lazy_download = f"{URL}{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+            fileName = quote_plus(get_name(lazy_file))
+            await lazy_file.reply_text(
+                text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
+                quote=True,
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("web Download", url=lazy_download),  # we download Link
+                                                    InlineKeyboardButton('▶Stream online', url=lazy_stream)]])  # web stream Link
+            )
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id,
                                               reply_markup=InlineKeyboardMarkup(
                                             [
                                                 [
-                                                  InlineKeyboardButton("Gen Link ", callback_data=f"generate_stream_link:{file_id}"),
+                                                  InlineKeyboardButton("Fast Download", url=lazy_download),
+                                                  InlineKeyboardButton("▶Stream online", url=lazy_stream),
                                                 ],
                                             ]),
                                             )
